c5bd5fd7e4f77489de522c2c2ea1930146bb6be4
Wayne Cen <wayne.cen@zerologix.com>
2024-07-13 14:22:30 +1000

    Setup: Initial project with using create-next-app

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+
+export const metadata: Metadata = {
+  title: "Qantas Hotels",
+  description: "Earn or Use Qantas Points when you book hotels and accommodation via Qantas Hotels. Book last minute hotel deals, cheap hotels and luxury stay here.",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
